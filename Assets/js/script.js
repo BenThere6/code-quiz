091a82e829f6cfe59962fb7d7a55d8923c6b2f37
BenThere <benjaminbirdsall@icloud.com>
2023-07-21 17:33:50 -0600

    Added listeners and functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,108 @@
 var viewScores = document.getElementById("view_scores");
+var timerCount = document.getElementById("timer_count");
 var mainScreen = document.getElementById("main_screen");
-var startQuizButton = document.getElementById("start_quiz");
+var startQuizBtn = document.getElementById("start_quiz");
 var questionScreen = document.getElementById("question_screen");
 var questionEl = document.getElementById("question");
 var optionOneEl = document.getElementById("option_one");
 var optionTwoEl = document.getElementById("option_two");
 var optionThreeEl = document.getElementById("option_three");
 var optionFourEl = document.getElementById("option_four");
+var optionOneBtn = document.getElementById("option_one_btn");
+var optionTwoBtn = document.getElementById("option_two_btn");
+var optionThreeBtn = document.getElementById("option_three_btn");
+var optionFourBtn = document.getElementById("option_four_btn");
 var rightOrWrongEl = document.getElementById("view_scores");
 var quizEndScreen = document.getElementById("quiz_end_screen");
 var finalScoreEl = document.getElementById("final_score");
 var scoresScreen = document.getElementById("scores_screen");
 var scoresListEl = document.getElementById("scores_list");
-var backButton = document.getElementById("go_back");
-var clearScoresButton = document.getElementById("clear_scores");
+var backBtn = document.getElementById("go_back");
+var clearScoresBtn = document.getElementById("clear_scores");
+var questionsArray = ['Commonly used data types DO NOT include:','The condition in an if / else statement is enclosed within _____.','Arrays in JavaScript can be used to store _____.','String values must be enclosed within _____ when being assigned to variables.','A very useful tool used during development and debugging for printing content to the debugger is:'];
+var optionsArray = [['strings','booleans','alerts','numbers'],['quotes','curly brackets','parenthesis','square brackets'],['numbers and strings','other arrays','booleans','all of the above'],['commas','curly brackets','quotes','parenthesis'],['JavaScript','terminal / bash','for loops','console.log']];
+var correctAnswers = ['alerts','parenthesis','all of the above','quotes','console.log'];
+var questionNum = 0;
+var points = 0;
+var isQuizComplete = false;
+
+finalScoreEl.textContent = points;
+
+startQuizBtn.addEventListener('click',function() {
+    renderQuestionAndOptions();
+});
+
+optionOneBtn.addEventListener('click',function() {
+    if (optionOneEl.textContent === correctAnswers[questionNum]) {
+        console.log("correct answer");
+        points += 20;
+    } else {
+        console.log("wrong answer");
+    }
+    questionNum++;
+    renderQuestionAndOptions();
+    renderScore();
+});
+
+optionTwoBtn.addEventListener('click',function() {
+    if (optionTwoEl.textContent === correctAnswers[questionNum]) {
+        console.log("correct answer");
+        points += 20;
+    } else {
+        console.log("wrong answer");
+    }
+    questionNum++;
+    renderQuestionAndOptions();
+    renderScore();
+});
+
+optionThreeBtn.addEventListener('click',function() {
+    if (optionThreeEl.textContent === correctAnswers[questionNum]) {
+        console.log("correct answer");
+        points += 20;
+    } else {
+        console.log("wrong answer");
+    }
+    questionNum++;
+    renderQuestionAndOptions();
+    renderScore();
+});
+
+optionFourBtn.addEventListener('click',function() {
+    if (optionFourEl.textContent === correctAnswers[questionNum]) {
+        console.log("correct answer");
+        points += 20;
+    } else {
+        console.log("wrong answer");
+    }
+    questionNum++;
+    renderQuestionAndOptions();
+    renderScore();
+});
+
+backBtn.addEventListener('click',function() {
+    questionNum = 0;
+    points = 0;
+    renderQuestionAndOptions();
+    renderScore();
+});
+
+function renderQuestionAndOptions() {
+    if (questionNum < 5) {
+        questionEl.textContent = questionsArray[questionNum];
+        optionOneEl.textContent = optionsArray[questionNum][0];
+        optionTwoEl.textContent = optionsArray[questionNum][1];
+        optionThreeEl.textContent = optionsArray[questionNum][2];
+        optionFourEl.textContent = optionsArray[questionNum][3];
+    }
+}
+
+function renderScore() {
+    finalScoreEl.textContent = points;
+}
+
+function checkQuizComplete() {
+    if (questionNum == 5) {
+        isQuizComplete = true;
+    }
+}
